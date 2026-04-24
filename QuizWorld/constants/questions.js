9bf66eefef78e38cb163249451cbc125b5ad1bed
diallosidymohamed886-design <diallosidymohamed886@gmail.com>
2026-04-24 21:16:47 +0000

    Create questions.js

---
+++ QuizWorld/constants/questions.js
@@ -0,0 +1,14 @@
+export const QUESTIONS = [
+  {
+    question: "Which animal is the slowest?",
+    options: ["Shark", "Bear", "Sloth", "Elephant"],
+    answer: "Sloth",
+    reward: 100
+  },
+  {
+    question: "What is the coldest continent?",
+    options: ["Africa", "South America", "Australia", "Antarctica"],
+    answer: "Antarctica",
+    reward: 200
+  }
+];
