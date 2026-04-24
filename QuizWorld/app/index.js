fe07a094f971a8fdf3f1a84ed129a8bc0c76ef20
diallosidymohamed886-design <diallosidymohamed886@gmail.com>
2026-04-24 21:14:02 +0000

    Update index.js

--- QuizWorld/app/index.js
+++ QuizWorld/app/index.js
@@ -1,1 +1,26 @@
-bbg
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
+
+export default function Home() {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>QuizWorld 🌍</Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => router.push("/quiz")}
+      >
+        <Text style={styles.text}>Jouer</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#0A0F2C" },
+  title: { fontSize: 30, color: "white", marginBottom: 20 },
+  button: { backgroundColor: "#FFD700", padding: 15, borderRadius: 10 },
+  text: { fontWeight: "bold" }
+});
