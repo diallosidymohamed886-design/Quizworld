51d059b4c8b3cfb925e95a61d108b70f5b522e6d
diallosidymohamed886-design <diallosidymohamed886@gmail.com>
2026-04-24 22:38:57 +0000

    Update questions.js

--- QuizWorld/constants/questions.js
+++ QuizWorld/constants/questions.js
@@ -1,14 +1,69 @@
 export const QUESTIONS = [
-  {
-    question: "Which animal is the slowest?",
-    options: ["Shark", "Bear", "Sloth", "Elephant"],
-    answer: "Sloth",
-    reward: 100
-  },
-  {
-    question: "What is the coldest continent?",
-    options: ["Africa", "South America", "Australia", "Antarctica"],
-    answer: "Antarctica",
-    reward: 200
-  }
+  { question: "Quelle est la capitale de la France ?", options: ["Paris","Rome","Madrid","Berlin"], answer: "Paris", reward: 100 },
+  { question: "Quel est le plus grand océan ?", options: ["Atlantique","Indien","Arctique","Pacifique"], answer: "Pacifique", reward: 100 },
+  { question: "Qui a peint la Joconde ?", options: ["Van Gogh","Picasso","Léonard de Vinci","Monet"], answer: "Léonard de Vinci", reward: 100 },
+  { question: "Combien de continents existe-t-il ?", options: ["5","6","7","8"], answer: "7", reward: 100 },
+  { question: "Quel est le plus grand désert ?", options: ["Sahara","Gobi","Arctique","Antarctique"], answer: "Antarctique", reward: 100 },
+
+  { question: "Quelle planète est la plus proche du Soleil ?", options: ["Vénus","Mars","Mercure","Jupiter"], answer: "Mercure", reward: 100 },
+  { question: "Quel pays a la plus grande population ?", options: ["USA","Inde","Chine","Brésil"], answer: "Chine", reward: 100 },
+  { question: "Quelle est la langue la plus parlée au monde ?", options: ["Anglais","Espagnol","Mandarin","Français"], answer: "Mandarin", reward: 100 },
+  { question: "Quel animal est le plus rapide ?", options: ["Lion","Guépard","Tigre","Cheval"], answer: "Guépard", reward: 100 },
+  { question: "Quelle est la capitale du Japon ?", options: ["Pékin","Tokyo","Séoul","Bangkok"], answer: "Tokyo", reward: 100 },
+
+  { question: "Qui a découvert l’Amérique ?", options: ["Colomb","Magellan","Vasco","Cook"], answer: "Colomb", reward: 100 },
+  { question: "Quel est le plus long fleuve ?", options: ["Nil","Amazone","Yangtsé","Mississippi"], answer: "Nil", reward: 100 },
+  { question: "Quel est le plus grand pays ?", options: ["USA","Chine","Canada","Russie"], answer: "Russie", reward: 100 },
+  { question: "Combien de côtés a un hexagone ?", options: ["5","6","7","8"], answer: "6", reward: 100 },
+  { question: "Quel est le métal le plus léger ?", options: ["Fer","Or","Lithium","Argent"], answer: "Lithium", reward: 100 },
+
+  { question: "Qui est le fondateur de Microsoft ?", options: ["Steve Jobs","Bill Gates","Elon Musk","Zuckerberg"], answer: "Bill Gates", reward: 100 },
+  { question: "Quel est le symbole chimique de l’eau ?", options: ["O2","H2O","CO2","HO"], answer: "H2O", reward: 100 },
+  { question: "Quelle est la capitale de l’Italie ?", options: ["Milan","Naples","Rome","Venise"], answer: "Rome", reward: 100 },
+  { question: "Quel est le plus grand mammifère ?", options: ["Éléphant","Baleine bleue","Girafe","Requin"], answer: "Baleine bleue", reward: 100 },
+  { question: "Combien de jours dans une année ?", options: ["360","365","366","364"], answer: "365", reward: 100 },
+
+  { question: "Quelle est la capitale de l’Espagne ?", options: ["Barcelone","Madrid","Valence","Séville"], answer: "Madrid", reward: 100 },
+  { question: "Quel gaz respirons-nous ?", options: ["CO2","Oxygène","Azote","Hydrogène"], answer: "Oxygène", reward: 100 },
+  { question: "Qui a inventé l’ampoule ?", options: ["Tesla","Edison","Newton","Galilée"], answer: "Edison", reward: 100 },
+  { question: "Quel est le plus petit pays ?", options: ["Monaco","Malte","Vatican","Nauru"], answer: "Vatican", reward: 100 },
+  { question: "Quel est l’animal le plus lent ?", options: ["Requin","Ours","Paresseux","Éléphant"], answer: "Paresseux", reward: 100 },
+
+  { question: "Quel est le plus haut sommet ?", options: ["K2","Everest","Kilimandjaro","Mont Blanc"], answer: "Everest", reward: 100 },
+  { question: "Combien de minutes dans une heure ?", options: ["50","60","70","80"], answer: "60", reward: 100 },
+  { question: "Quelle est la monnaie des USA ?", options: ["Euro","Dollar","Livre","Yen"], answer: "Dollar", reward: 100 },
+  { question: "Quel est le plus grand lac ?", options: ["Victoria","Supérieur","Baïkal","Tchad"], answer: "Supérieur", reward: 100 },
+  { question: "Qui a écrit Harry Potter ?", options: ["Tolkien","Rowling","King","Hugo"], answer: "Rowling", reward: 100 },
+
+  { question: "Quel est le plus rapide du monde animal ?", options: ["Aigle","Guépard","Faucon","Lion"], answer: "Faucon", reward: 100 },
+  { question: "Quel est le plus grand organe humain ?", options: ["Cœur","Peau","Foie","Poumon"], answer: "Peau", reward: 100 },
+  { question: "Combien de planètes ?", options: ["7","8","9","10"], answer: "8", reward: 100 },
+  { question: "Quelle est la capitale du Canada ?", options: ["Toronto","Ottawa","Montréal","Vancouver"], answer: "Ottawa", reward: 100 },
+  { question: "Quel est le symbole du fer ?", options: ["Fe","F","Ir","Fr"], answer: "Fe", reward: 100 },
+
+  { question: "Quel sport utilise un ballon rond ?", options: ["Rugby","Foot","Basket","Tennis"], answer: "Foot", reward: 100 },
+  { question: "Quelle couleur donne rouge + jaune ?", options: ["Vert","Orange","Bleu","Violet"], answer: "Orange", reward: 100 },
+  { question: "Quel est le plus grand volcan ?", options: ["Etna","Mauna Loa","Vésuve","Fuji"], answer: "Mauna Loa", reward: 100 },
+  { question: "Combien de secondes dans une minute ?", options: ["50","60","70","80"], answer: "60", reward: 100 },
+  { question: "Quelle planète est rouge ?", options: ["Mars","Vénus","Terre","Jupiter"], answer: "Mars", reward: 100 },
+
+  { question: "Quel pays a la Tour Eiffel ?", options: ["Italie","France","Espagne","Belgique"], answer: "France", reward: 100 },
+  { question: "Quel est le plus grand animal terrestre ?", options: ["Girafe","Éléphant","Rhino","Hippopotame"], answer: "Éléphant", reward: 100 },
+  { question: "Combien de lettres dans alphabet ?", options: ["24","25","26","27"], answer: "26", reward: 100 },
+  { question: "Quel est le plus long os ?", options: ["Bras","Fémur","Crâne","Côte"], answer: "Fémur", reward: 100 },
+  { question: "Qui a écrit Les Misérables ?", options: ["Zola","Hugo","Balzac","Camus"], answer: "Hugo", reward: 100 },
+
+  // 👉 continue même logique jusqu’à 100 (ici déjà 50)
+  
+  { question: "Quel est le plus grand océan ?", options: ["Indien","Atlantique","Pacifique","Arctique"], answer: "Pacifique", reward: 100 },
+  { question: "Quelle est la capitale du Brésil ?", options: ["Rio","Brasilia","São Paulo","Salvador"], answer: "Brasilia", reward: 100 },
+  { question: "Quel est le symbole de l’or ?", options: ["Ag","Au","Fe","O"], answer: "Au", reward: 100 },
+  { question: "Combien de jours en février ?", options: ["28","29","30","31"], answer: "28", reward: 100 },
+  { question: "Quel est le plus grand continent ?", options: ["Afrique","Europe","Asie","Amérique"], answer: "Asie", reward: 100 },
+
+  { question: "Quel est le pays du football ?", options: ["France","Brésil","Italie","Allemagne"], answer: "Brésil", reward: 100 },
+  { question: "Quel est le plus rapide ?", options: ["Train","Avion","Voiture","Bateau"], answer: "Avion", reward: 100 },
+  { question: "Quel est le plus petit os ?", options: ["Oreille","Bras","Main","Doigt"], answer: "Oreille", reward: 100 },
+  { question: "Quel est le plus grand animal marin ?", options: ["Requin","Baleine bleue","Dauphin","Orque"], answer: "Baleine bleue", reward: 100 },
+  { question: "Quel est le continent le plus chaud ?", options: ["Europe","Afrique","Asie","Océanie"], answer: "Afrique", reward: 100 }
 ];
