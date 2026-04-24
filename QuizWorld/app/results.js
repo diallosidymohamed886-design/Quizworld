31cbb66f63c8a73e621da17c3fc3f5f643b79c56
diallosidymohamed886-design <diallosidymohamed886@gmail.com>
2026-04-24 21:15:08 +0000

    Update results.js

--- QuizWorld/app/results.js
+++ QuizWorld/app/results.js
@@ -1,1 +1,17 @@
-vhf
+import { View, Text, TouchableOpacity } from "react-native";
+import { useLocalSearchParams, useRouter } from "expo-router";
+
+export default function Results() {
+  const { money } = useLocalSearchParams();
+  const router = useRouter();
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 30 }}>💰 {money}</Text>
+
+      <TouchableOpacity onPress={() => router.push("/")}>
+        <Text>Rejouer</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
